376e9f553ab6329770203f1b1a60ec20b2820e5b
lee-vincent <vinnie@vinnielee.io>
2024-01-27 11:44:58 -0500

    mcc

--- tests.py
+++ tests.py
@@ -14,22 +14,10 @@ from credit_card_validator import credit_card_validator
 #   Length: 15
 #   valid Luhn algorithm check bit
 
-def is_valid_luhn(digits):
-    check_digit = 0
-
-    for i in range(len(digits) - 2, -1, -1):
-        if i % 2 == 0:
-            doubled_digit = digits[i] * 2
-            check_digit += doubled_digit - 9 if doubled_digit > 9 else doubled_digit
-        else:
-            check_digit += digits[i]
-
-    return (10 - (check_digit % 10)) % 10 == digits[-1]
-
-# Example usage:
-digits = [4, 5, 1, 2, 3, 6, 7, 8, 9, 0, 1]
-result = is_valid_luhn(digits)
-print(result)  # True or False depending on the input digits
+# MasterCard
+# 5574726700232866
+# 2720926415343830
+# 2221858293648259
 
 
 class TestCreditCardValidator(unittest.TestCase):
@@ -40,19 +28,13 @@ class TestCreditCardValidator(unittest.TestCase):
     def test_invalidprefix(self):
         self.assertFalse(credit_card_validator(7))
 
-    def test_mastercardprefix51_56(self):
+    def test_mastercard_prefix51_55_checkbit(self):
         # test correct prefix and length but wrong check bit
-        for i in range(51, 55):
-            with self.subTest(i=i):
-                cc_num = str(i)+"00000000000000"
-                self.assertFalse(credit_card_validator(cc_num))
+        self.assertFalse(credit_card_validator(5574726700232860))
 
-    def test_mastercardprefix2221_2720(self):
+    def test_mastercard_prefix2221_2720_checkbit(self):
         # test correct prefix and length but wrong check bit
-        for i in range(2221, 2720):
-            with self.subTest(i=i):
-                cc_num = str(i)+"000000000000"
-                self.assertFalse(credit_card_validator(cc_num))
+        self.assertFalse(credit_card_validator(2221858293648250))
 
     def test_prefix_length15(self):
         # invalid prefix, correct check bit
