425c7d94da9405ded876bf9e3f1e909b8483184a
lee-vincent <vinnie@vinnielee.io>
2024-01-27 12:06:36 -0500

    remo

--- tests.py
+++ tests.py
@@ -49,21 +49,21 @@ class TestCreditCardValidator(unittest.TestCase):
         # test correct prefix and length but wrong check bit
         self.assertFalse(credit_card_validator(372492094099162))
 
-    def test_noinput(self):
-        self.assertFalse(credit_card_validator(0))
+    # def test_noinput(self):
+    #     self.assertFalse(credit_card_validator(0))
 
-    def test_invalidprefix(self):
-        self.assertFalse(credit_card_validator(7))
+    # def test_invalidprefix(self):
+    #     self.assertFalse(credit_card_validator(7))
 
-    def test_prefix_length15(self):
-        # invalid prefix, correct check bit
-        self.assertFalse(credit_card_validator(700000000000005))
+    # def test_prefix_length15(self):
+    #     # invalid prefix, correct check bit
+    #     self.assertFalse(credit_card_validator(700000000000005))
 
-    def test_prefix_length16(self):
-        self.assertFalse(credit_card_validator(7000000000000000))
+    # def test_prefix_length16(self):
+    #     self.assertFalse(credit_card_validator(7000000000000000))
 
-    def test_prefix_length17(self):
-        self.assertFalse(credit_card_validator(70000000000000000))
+    # def test_prefix_length17(self):
+    #     self.assertFalse(credit_card_validator(70000000000000000))
 
 
 if __name__ == '__main__':
